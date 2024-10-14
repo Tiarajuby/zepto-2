90622f7d32d978b7597f8fc8c3f066752a62bfc9
Tiara Juby <tiaramerlinjuby@Tiaras-MacBook-Pro.local>
2024-10-14 18:57:18 +0530

    Add initial project files

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,101 @@
+import orderModel from './../models/orderModel.js';
+import userModel from './../models/userModel.js';
+import Razorpay from "razorpay";
+import crypto from 'crypto';
+
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET_KEY,
+});
+
+// Placing user order for frontend
+const placeOrder = async (req, res) => {
+  const frontend_url = 'http://localhost:5173';
+  try {
+    const newOrder = new orderModel({
+      userId: req.body.userId,
+      items: req.body.items,
+      amount: req.body.amount,
+      address: req.body.address,
+    });
+
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+
+    const amountInINR = req.body.amount * 100; // Amount in paisa (100 paisa = 1 INR)
+
+    const options = {
+      amount: amountInINR, // Amount in paisa
+      currency: "INR",
+      receipt: newOrder._id.toString(),
+    };
+
+    const razorpayOrder = await razorpay.orders.create(options);
+
+    res.json({
+      success: true,
+      orderId: newOrder._id,
+      razorpayOrderId: razorpayOrder.id,
+      amount: amountInINR,
+      key: process.env.RAZORPAY_KEY_ID, // Send the Razorpay public key to frontend
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+const verifyOrder = async (req, res) => {
+  const { orderId, razorpayPaymentId, razorpayOrderId, razorpaySignature } = req.body;
+ 
+
+  const generatedSignature = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET_KEY)
+    .update(razorpayOrderId + "|" + razorpayPaymentId)
+    .digest("hex");
+
+  if (generatedSignature === razorpaySignature) {
+    try {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Payment verified" });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false, message: "Error" });
+    }
+  } else {
+    await orderModel.findByIdAndDelete(orderId);
+    res.json({ success: false, message: "Payment verification failed" });
+  }
+};
+
+const userOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+const updateStatus = async (req, res) => {
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status });
+    res.json({ success: true, message: "Status Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus };
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Craving made easy—order in, dig in, and savor the moment with every bite!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
